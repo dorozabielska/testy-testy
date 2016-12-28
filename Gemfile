ab5e533ee220295bcdd455cf9fa2b5615e3481bf
doro <doro.zabielska@gmail.com>
2016-12-28 18:22:25 +0100

    pierwszy-commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+#ruby '2.1.5'
+source 'https://rubygems.org'
+
+gem 'bundler'
+gem 'rspec'
+gem 'selenium-webdriver'
+gem 'test-unit'
+gem 'faker'
---
+++ spec/home_spec.rb
@@ -0,0 +1,46 @@
+require 'selenium-webdriver'
+require 'rspec'
+require 'test-unit'
+require 'test/unit/assertions'
+require 'faker'
+
+include Test::Unit::Assertions
+
+class FullPageTest
+
+  describe 'QA site test' do
+
+    before(:each) do
+      @driver = Selenium::WebDriver.for :chrome
+      @base_url = 'http://www.demoqa.com'
+      @accept_next_alert = true
+      @driver.manage.timeouts.implicit_wait = 10
+    end
+
+    after(:each) do
+      @driver.quit
+    end
+
+    it 'changes the page to about us' do
+      @driver.get(@base_url + '/')
+      @driver.find_element(:id, 'menu-item-158').click
+      assert_equal @driver.find_element(:class => 'entry-title').text, 'About us'
+      assert_equal @driver.title, 'About us | Demoqa'
+    end
+
+    it 'tests registration form' do
+      @driver.get(@base_url + '/registration')
+      @driver.find_element(:id, 'name_3_firstname')
+          .send_keys(Faker::Name.unique.name + ' ' + Faker::Number.number(10))
+    end
+
+    it 'checks if the hover is working', :start do
+      @driver.get(@base_url)
+      assert_true(@driver.find_elements(:class, 'ui-tooltip-content').empty?)
+      @driver.action.move_to(@driver.find_element(id: 'menu-item-158'), 10, 10).perform
+      assert_true(@driver.find_elements(:class, 'ui-tooltip-content').any?)
+    end
+
+  end
+
+end
