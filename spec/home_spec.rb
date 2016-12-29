41ff1620e94fd7aec21515da3cb51585e96103d1
doro <doro.zabielska@gmail.com>
2016-12-29 08:47:20 +0100

    registration-positive-docne

--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -28,10 +28,28 @@ class FullPageTest
       assert_equal @driver.title, 'About us | Demoqa'
     end
 
-    it 'tests registration form' do
+    it 'tests registration form', :registration, :positive do
       @driver.get(@base_url + '/registration')
       @driver.find_element(:id, 'name_3_firstname')
-          .send_keys(Faker::Name.unique.name + ' ' + Faker::Number.number(10))
+        .send_keys(Faker::Name.unique.first_name + ' ' + Faker::Number.number(10))
+      @driver.find_element(:id, 'name_3_lastname')
+        .send_keys(Faker::Name.unique.last_name + ' ' + Faker::Number.number(10))
+      #TODO: find something better than xpath
+      @driver.find_element(:xpath, "//*[@id='pie_register']/li[3]/div/div/input[2]").click
+      @driver.find_element(:id, 'phone_9')
+        .send_keys('48' + Faker::Number.number(9))
+      @driver.find_element(:id, 'username')
+        .send_keys(Faker::Name.unique.name + ' ' + Faker::Number.number(10))
+      @driver.find_element(:id, 'email_1')
+        .send_keys('doro.zabielska' + '+' +Faker::Number.number(10) + '@gmail.com')
+      password=Faker::Internet.password(8)
+      @driver.find_element(:id, 'password_2')
+        .send_keys(password)
+      @driver.find_element(:id, 'confirm_password_password_2')
+        .send_keys(password)
+      @driver.find_element(:name, 'pie_submit').click
+      assert_equal @driver.find_element(:class => 'piereg_message').text, 'Thank you for your registration'
+      #sleep(10)
     end
 
     it 'checks if the hover is working', :start do
